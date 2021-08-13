0cd618b7d5ee3cb4374339eb469f6455a8294fe7
Juan Carlos <jcgalvezocampo@gmail.com>
2021-08-12 19:04:01 -0500

    Creacion del archiv ConfigurarController, que se utilizará para las configuraciones de librerias externas de Yii

---
+++ controllers/ConfigurarController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessController;
+use yii\web\Controller;
+use yii\helpers\Html;
+
+class EstudianteController extends Controller
+{
+    public function actionIndex()
+    {
+        
+    }
+}
