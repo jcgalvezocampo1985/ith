a8668d9967bbd954d429c8e00fa6d5f7dc1addf1
Juan Carlos <jcgalvezocampo@gmail.com>
2021-08-16 15:17:39 -0500

    Modificación del reporte de boleta de calificación, se agregó columna de grupo en detalle de boleta

--- controllers/ReporteController.php
+++ controllers/ReporteController.php
@@ -132,7 +132,7 @@ class ReporteController extends Controller
                 $pdf->SetX(12);
                 $pdf->Cell(95, 5, utf8_decode($row['desc_materia']), 1, 0, 'L');
                 $pdf->Cell(20, 5, utf8_decode($row['cve_materia']), 1, 0, 'C');
-                $pdf->Cell(15, 5, utf8_decode(""), 1, 0, 'C');
+                $pdf->Cell(15, 5, utf8_decode($row["desc_grupo"]), 1, 0, 'C');
                 $pdf->Cell(15, 5, utf8_decode($row['opc_curso']), 1, 0, 'C');
                 $pdf->Cell(10, 5, utf8_decode($row['creditos']), 1, 0, 'C');
                 $pdf->Cell(40, 5, utf8_decode($row['calificacion']), 1, 0, 'C');
@@ -173,5 +173,133 @@ class ReporteController extends Controller
 
             $pdf->Output('D',$idestudiante.'_'.$periodo.'.pdf');
             exit;
+    }
+
+    public function actionHorario()
+    {
+            $idestudiante = Html::encode( $_REQUEST['id']);
+            $sql_encabezado = "SELECT
+                                    *
+                                FROM
+                                    boleta_estudiante_encabezado
+                                WHERE
+                                idestudiante=:idestudiante";
+            $encabezado = Yii::$app->db->createCommand($sql_encabezado)
+                                ->bindValue(':idestudiante', $idestudiante)
+                                ->queryOne();
+
+            $sql_materias = "SELECT
+                                *
+                            FROM
+                                boleta_detalle_v
+                             WHERE
+                                idestudiante =:idestudiante";
+            $cuerpo = Yii::$app->db->createCommand($sql_materias)
+                                   ->bindValue(':idestudiante', $idestudiante)
+                                   ->queryAll();
+
+            $periodo = utf8_decode($encabezado['desc_ciclo']);
+            $fecha = date('Y-m-d');
+            $no_control = $encabezado['idestudiante'];
+            $estudiante = $encabezado['nombre_estudiante'];
+            $semestre = $encabezado['num_semestre'];
+            $carrera = $encabezado['desc_carrera'];
+            $especialidad = '';
+            $plan = $encabezado['plan_estudios'];
+
+            header('Content-type: application/pdf');
+            $pdf = new PDF();
+            $pdf->AliasNbPages();
+            $pdf->AddPage('P', 'Letter');
+            $pdf->AddFont('Montserrat-SemiBold', '', 'Montserrat-SemiBold.php');
+            $pdf->AddFont('Montserrat-Bold', '', 'Montserrat-Bold.php');
+            $pdf->AddFont('Montserrat-MediumItalic', '', 'Montserrat-MediumItalic.php');
+            $pdf->AddFont('Montserrat-LightItalic', '', 'Montserrat-LightItalic.php');
+            $pdf->AddFont('Montserrat-Bold', '', 'Montserrat-Bold.php');
+            $pdf->AddFont('Montserrat-Regular', '', 'Montserrat-Regular.php');
+        
+            $pdf->SetFont('Montserrat-SemiBold', '', 8);
+            $pdf->SettextColor(0, 0, 0);
+            $pdf->Text(12, 60, utf8_decode('BOLETA DE CALIFICACIONES AL PERIODO:'));
+            $pdf->Text(78, 60, $periodo);
+            $pdf->Text(125, 60, 'FECHA:');
+            $pdf->Text(138, 60, $fecha);
+            $pdf->Text(12, 70, utf8_decode('NÚMERO DE CONTROL:'));
+            $pdf->Text(50, 70, utf8_decode($no_control));
+            $pdf->Text(12, 75, utf8_decode('ESTUDIANTE:'));
+            $pdf->Text(35, 75, utf8_decode($estudiante));
+            //$pdf->Text(12, 80, utf8_decode('SEMESTRE:'));
+            //$pdf->Text(32, 80, utf8_decode($semestre));
+            //$pdf->Text(12, 85, utf8_decode('CARRERA:'));
+            //$pdf->Text(30, 85, utf8_decode($carrera));
+            $pdf->Text(12, 80, utf8_decode('CARRERA:'));
+            $pdf->Text(32, 80, utf8_decode($carrera));
+            //$pdf->Text(12, 90, utf8_decode('ESPECIALIDAD:'));
+            //$pdf->Text(38, 90, utf8_decode($especialidad));
+            //$pdf->Text(12, 95, utf8_decode('PLAN:'));
+            //$pdf->Text(24, 95, utf8_decode($plan));
+            $pdf->Text(12, 85, utf8_decode('PLAN:'));
+            $pdf->Text(30, 85, utf8_decode($plan));
+
+            $pdf->SetFont('Montserrat-Bold', '', 8);
+            $pdf->SetXY(8, 90);
+            $pdf->Cell(75, 7, "MATERIA / DOCENTE", 1, 0, 'C');
+            $pdf->Cell(13, 7, "CLAVE", 1, 0, 'C');
+            $pdf->Cell(12, 7, "GRUPO", 1, 0, 'C');
+            $pdf->Cell(9, 7, "REP", 1, 0, 'C');
+            $pdf->Cell(7, 7, "CR", 1, 0, 'C');
+            $pdf->SetFont('Montserrat-Bold', '', 6);
+            $pdf->Cell(14, 7, "LUNES", 1, 0, 'C');
+            $pdf->Cell(14, 7, "MARTES", 1, 0, 'C');
+            $pdf->Cell(14, 7, utf8_decode("MIÉRCOLES"), 1, 0, 'C');
+            $pdf->Cell(14, 7, "JUEVES", 1, 0, 'C');
+            $pdf->Cell(14, 7, "VIERNES", 1, 0, 'C');
+            $pdf->Cell(14, 7, utf8_decode("SÁBADO"), 1, 0, 'C');
+            $pdf->Ln();
+
+            foreach($cuerpo as $row)
+            {
+                $pdf->SetX(8);
+                $pdf->SetFont('Montserrat-Regular', '', 6);
+                $pdf->MultiCell(75, 5, utf8_decode($row['desc_materia'])."\n-M.C. RAQUEL JIMENEZ RAMIREZ", 1, 'L', 0);
+
+                $y = $pdf->GetY() - 10;
+                $pdf->SetXY(83, $y);
+
+                $pdf->Cell(13, 10, utf8_decode($row['cve_materia']), 1, 0, 'C');
+                $pdf->Cell(12, 10, utf8_decode(""), 1, 0, 'C');
+                $pdf->Cell(9, 10, utf8_decode($row['opc_curso']), 1, 0, 'C');
+                $pdf->Cell(7, 10, utf8_decode($row['creditos']), 1, 0, 'C');
+                $pdf->Cell(14, 10, "8:00-11:00", 1, 0, 'C');
+                $pdf->Cell(14, 10, "8:00-11:00", 1, 0, 'C');
+                $pdf->Cell(14, 10, "8:00-11:00", 1, 0, 'C');
+                $pdf->Cell(14, 10, "8:00-11:00", 1, 0, 'C');
+                $pdf->Cell(14, 10, "8:00-11:00", 1, 0, 'C');
+                $pdf->Cell(14, 10, "8:00-11:00", 1, 0, 'C');
+
+                $pdf->Ln();
+            }
+
+            $pdf->SetFont('Montserrat-Bold', '', 10);
+            $pdf->Text(12, 185, 'A T E N T A M E N T E');
+            $pdf->SetFont('Montserrat-MediumItalic', '', 8);
+            $pdf->Text(12, 189, utf8_decode('Excelencia en Educación Tecnológica®'));
+            $pdf->SetFont('Montserrat-LightItalic', '', 8);
+            $pdf->Text(12, 193, utf8_decode('"DONDE MORA EL SABER MORA LA PATRIA"®'));
+            $pdf->SetFont('Montserrat-Bold', '', 8);
+            $pdf->Text(12, 230, utf8_decode('MANUEL ERNESTO VILLALOBOS LÓPEZ'));
+            $pdf->Text(12, 234, utf8_decode('JEFE DEL DEPARTAMENTO DE SERVICIOS ESCOLARES'));
+            $pdf->SetFont('Montserrat-Regular', '', 7);
+            $pdf->Text(12, 238, utf8_decode('C.c.p. SE'));
+            $pdf->Text(12, 242, utf8_decode('L.I. MEVL/'));
+
+            $url_firma = Yii::$app->basePath."/web/img/firma.png";
+            $pdf->Image($url_firma, 40, 190, 45);
+
+            $url_firma = Yii::$app->basePath."/web/img/sello.png";
+            $pdf->Image($url_firma, 130, 190, 45);
+
+            $pdf->Output('I',$idestudiante.'_'.$periodo.'.pdf');
+            //exit;
     }
 }
--- views/estudiante/horario.php
+++ views/estudiante/horario.php
@@ -42,7 +42,7 @@ $f = ActiveForm::begin([
                         <td><?= $row['nombre_estudiante'] ?></td>
                         <td><?= $row['desc_carrera'] ?></td>
                         <td><?= $row['desc_ciclo'] ?></td>
-                        <td><a href="<?= Url::toRoute(['reporte/boleta/', 'id' => $row['idestudiante']]) ?>" class="btn btn-success" href="#" role="button">Descargar</a></td>
+                        <td><a href="<?= Url::toRoute(['reporte/horario/', 'id' => $row['idestudiante']]) ?>" class="btn btn-success" role="button">Descargar</a></td>
                     </tr>
                     <?php endforeach ?>
                 </tbody>
